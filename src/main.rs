ae0f4067db08d0f973e16d04ed9fb9e2f79fdfaf
Xavier Lange <xrlange@gmail.com>
2020-02-10 08:23:38 -0800

    now with more speed and coolness

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
+#[macro_use]
+extern crate serde;
+
+#[derive(Deserialize, Debug)]
+struct IP {
+    ip: String,
+}
+
 #[tokio::main]
 async fn main() {
     let client = reqwest::Client::new();
-    let request = client.get("https://google.com");
-    let result = request.send().await.unwrap();
-    let body = result.bytes().await.unwrap();
-    println!("{:?}", body);
+    let result = reqwest::get("http://ip.jsontest.com")
+        .await
+        .unwrap()
+        .json::<IP>()
+        .await
+        .unwrap();
+
+    println!("{:#?}", result);
+}
+
+#[tokio::main]
+async fn a_parallel_main() {
+    let mut tasks = vec![];
+
+    for _ in 1..10 {
+        tasks.push(tokio::spawn(async {
+            let client = reqwest::Client::new();
+            let result = client.get("https://google.com").send().await.unwrap();
+            let body = result.bytes().await.unwrap();
+            println!("body: {:?}", body);
+        }));
+    }
+
+    futures::future::join_all(tasks).await;
 }
